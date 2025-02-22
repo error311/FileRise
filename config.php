89ce86f2c2c110dd7936a79fcf3f4a401242c241
Ryan <ryantl66@gmail.com>
2025-02-21 20:41:57 -0500

    changed time stamp and location

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php
-define('UPLOAD_DIR', '/var/www/html/upload/');
-define('BASE_URL', 'https://yourwebsite.com/upload/');
+define('UPLOAD_DIR', '/var/www/uploads/');
+define('BASE_URL', 'http://yourwebsite/uploads/');
 define('TIMEZONE', 'America/New_York');
-define('DATE_TIME_FORMAT', 'm/d/y  H:i');
+define('DATE_TIME_FORMAT', 'm/d/y  h:iA');
 define('TOTAL_UPLOAD_SIZE', '20G');
 date_default_timezone_set(TIMEZONE);
 ?>
