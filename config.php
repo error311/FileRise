7c689f4b7562f05fed7817da353e79bf7c6dc0fb
Ryan <ryantl66@gmail.com>
2025-02-23 21:00:12 -0500

    5gb

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ define('UPLOAD_DIR', '/var/www/uploads/');
 define('BASE_URL', 'http://yourwebsite/uploads/');
 define('TIMEZONE', 'America/New_York');
 define('DATE_TIME_FORMAT', 'm/d/y  h:iA');
-define('TOTAL_UPLOAD_SIZE', '20G');
+define('TOTAL_UPLOAD_SIZE', '5G');
 date_default_timezone_set(TIMEZONE);
 ?>
