850e42af15c6aa17114d090de6376cc4885420cf
Ryan <ryantl66@gmail.com>
2025-03-03 13:08:08 -0500

    users_dir path set

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('BASE_URL', 'http://yourwebsite/uploads/');
 define('TIMEZONE', 'America/New_York');
 define('DATE_TIME_FORMAT', 'm/d/y  h:iA');
 define('TOTAL_UPLOAD_SIZE', '5G');
-define('USERS_DIR', '/var/www/uploads/');
+define('USERS_DIR', '/var/www/users/');
 define('USERS_FILE', 'users.txt');
 date_default_timezone_set(TIMEZONE);
 ?>
